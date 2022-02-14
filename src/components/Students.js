1c20d38c94dec0c822d173bdb08d3fe436e78262
Holman Morales <hmoralesguido@gmail.com>
2022-02-13 22:27:06 -0600

    Creacion de una clase en el nuevo componente

---
+++ src/components/Students.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Students extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Students
